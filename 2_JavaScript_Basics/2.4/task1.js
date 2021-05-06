c02785827507837189123cf0a696b46614ecc052
Olesya Naibova <o.naibova@gmail.com>
2021-05-06 00:00:12 -0500

    Complete 2.4 tasks

---
+++ 2_JavaScript_Basics/2.4/task1.js
@@ -0,0 +1,11 @@
+/*
+1. Declare two variables: 'admin' and 'name'.
+2. Write the string 'John' to a variable name.
+3. Copy the value from the variable 'name' to 'admin'.
+4. Display the value 'admin' using the function alert(should show "John").
+*/
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
---
+++ 2_JavaScript_Basics/2.4/task2.js
@@ -0,0 +1,7 @@
+/* 
+1. Create a variable for the name of our planet. What would you call her?
+2. Create a variable to hold the name of the current site visitor. How would you name such a variable?
+*/
+
+let ourPlanetName;
+let currentSiteVisitor;
